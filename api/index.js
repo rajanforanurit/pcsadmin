171fca0aebe512045a99e50435bc4725bdd9360a
rajanforanurit <rajan.singh@anuritinnovation.com>
2026-06-25 21:52:54 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -79,7 +79,7 @@ const QuestionSchema = new mongoose.Schema({
   marks: { type: Number, default: 2 },
   negativeMarks: { type: Number, default: 0.66 },
   correct_answer: { type: Number, required: true },
-  explanation: {type: String, required :true},
+  explanation: {type: String},
   batchId: { type: String }
 }, {
   timestamps: true,
